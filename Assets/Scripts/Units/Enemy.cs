a58fa9bd2a66248821ec7c734955059cce77dfad
AlexAntonides <15697@ma-web.nl>
2014-12-08 22:49:10 +0100

    KEVIN'S PUSH: EnemyManager, WaveManager, EnemySpawnscript etc.

---
+++ Assets/Scripts/Units/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public int health;
+	public int speed;
+	public int damage;
+	public int currency;
+	void Start()
+	{
+		
+	}
+}
